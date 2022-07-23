9d7a985d3fc1345d4894a48de6dbc638b282b1c3
Md. Al Amin <aminspicse@gmail.com>
2022-07-23 09:09:46 +0600

    Python Functions

---
+++ 8function.py
@@ -0,0 +1,13 @@
+# Python Functions
+
+def myfun():
+    print("First Function")
+
+myfun()
+
+# Parameter as a function
+
+def pfunction(email):
+    print("Your Email: 😀 "+email)
+
+pfunction("aminspicse@gmail.com")
